7e750ed6711f75f63e2f7e77fb6233d84d673717
wightman <davidwightmanmusic@gmail.com>
2018-02-22 12:44:47 -0500

    attempt to get callback to only fire once

--- merlin.js
+++ merlin.js
@@ -1,15 +1,41 @@
-(function() {
-	var xhrOpen = XMLHttpRequest.prototype.open;
-	XMLHttpRequest.prototype.open = function() {
-		console.log('AJAX request started');
-		this.addEventListener('load', function() {
-			callWhenReadyToGo();
-			console.log('The AJAX response is: ' + this.responseText);
-		});
-		xhrOpen.apply(this, arguments);
-	};
-})();
+var newCallCount = 0;
+var hasBeenCalled = false;
+
+function newCall() {
+	if (hasBeenCalled) {
+		return;
+	} else {
+		hasBeenCalled = true;
+
+		console.log('the function newCall has been activated');
+		var xhrOpen = XMLHttpRequest.prototype.open;
+		XMLHttpRequest.prototype.open = function() {
+			console.log('AJAX request started');
+			this.addEventListener('load', function() {
+				console.log('The AJAX response is: ' + this.responseText);
+				console.log('AJAX call completed');
+				newCallCount++;
+			});
+
+			xhrOpen.apply(this, arguments);
+		};
+	}
+}
 
 function callWhenReadyToGo(callback) {
-	console.log('AJAX call completed');
+	console.log('The function callWhenReadyToGo has been activated');
+	console.log(newCallCount);
+	if (newCallCount === 0) {
+		newCall();
+		return;
+	} else {
+		console.log('NOOOOO!');
+		return;
+	}
+}
+
+callWhenReadyToGo(newCall);
+
+function theCallback() {
+	console.log('LOADED PAGE.');
 }
